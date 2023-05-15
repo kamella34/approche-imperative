5b403800f44bdd61c31cabeef2a830db7f6eac8d
kamella34 <vergnaud.kamella@bbox.fr>
2023-05-15 14:07:46 +0200

    first commit

---
+++ src/fr/declaration/variable/DeclarationApp.java
@@ -0,0 +1,28 @@
+package fr.declaration.variable;
+
+import java.sql.SQLOutput;
+
+public class DeclarationApp {
+    public static void main (String[] args){
+       byte by = 1;
+       short sh = -12;
+       int in = 123;
+       long lo = 1234L;
+       float fl = 1.3F;
+       double dbl = 111.99D;
+       char cc = 'a';
+       boolean bool = true;
+       String strg = "HelloWorld, donne moi des \u20ac";
+       String randomString = "Voici le résultat d’un calcul : %n 1+5=6";
+
+       System.out.println(by);
+       System.out.println(sh);
+       System.out.println(lo);
+       System.out.println(fl);
+       System.out.println(dbl);
+       System.out.println(cc);
+       System.out.println(bool);
+       System.out.println(strg);
+       System.out.println(randomString);
+    };
+}
