469e37f45843f5e687772b460e71982cb136efbc
kamella34 <vergnaud.kamella@bbox.fr>
2023-05-16 10:38:53 +0200

    commit boucle/tab

---
+++ src/fr/boucles/ExerciceBoucleBase.java
@@ -0,0 +1,20 @@
+package fr.boucles;
+
+public class ExerciceBoucleBase {
+    public static void main(String[] args) {
+        for (byte i = 0; i <= 10; i++) {
+            System.out.println(i);
+        }
+        byte a = 0;
+        while (a < 20) {
+            System.out.println("kamella");
+            a++;
+        }
+        for (byte i = 0; i <= 100; i += 2) {
+            System.out.println(i);
+        }
+        for (byte i = 1; i <= 100; i+=2){
+            System.out.println(i);
+        }
+    }
+}
