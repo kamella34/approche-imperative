daa578debdae8fee00c80cb62f77e115113ab475
kamella34 <vergnaud.kamella@bbox.fr>
2023-05-17 20:49:19 +0200

    commit somme

---
+++ src/fr/algorithmie/SommeDeTableaux.java
@@ -0,0 +1,22 @@
+package fr.algorithmie;
+
+public class SommeDeTableaux {
+    public static void main(String[] args) {
+        int[][] array = {
+                {1, 15, -3, 0, 8, 7, 4, -2, 28, 7, -1, 17, 2, 3, 0, 14, -4},
+                {-1, 12, 17, 14, 5, -9, 0, 18, -6, 0, 4, -13, 5, 7, -2, 8, -1}
+        };
+
+        int[] sommeArrayBidimentionel = new int[array[0].length];
+
+        for (int i = 0; i < array[0].length; i++) {
+            sommeArrayBidimentionel[i] = array[0][i] + array[1][i];
+        }
+
+        for (int value : sommeArrayBidimentionel) {
+            System.out.print(value + " ");
+        }
+    }
+}
+
+
